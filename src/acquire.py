3663cb5edcd1a5a5ee6c77b3ed59cffceb9aa46e
Ryan McCall <ryan.mccall@codeup.com>
2022-06-29 10:33:25 -0500

    Moved download steps into a separate function

--- src/acquire.py
+++ src/acquire.py
@@ -6,8 +6,34 @@ from kaggle.api.kaggle_api_extended import KaggleApi
 
 # Write supporting functions here
 
+def download_files(api, files_to_download, storage_location):
+    
+    #Loop over the files
+    for file in files_to_download:
+
+        #Send download request to kaggle api for that file from the amex
+        #competition
+        #This method automatically checks if you already have downloaded
+        #the most recent version of the data
+        api.competition_download_file(
+            competition='amex-default-prediction',
+            file_name=file,
+            path=storage_location
+        )
+
+        #Check if we already have extracted the csv from the zip
+        if not os.path.isfile(storage_location + '/' + file): 
+
+            path_to_zip_file = storage_location + '/' + file + '.zip'
+
+            #Open the zip file in read mode
+            with zipfile.ZipFile(path_to_zip_file, 'r') as zip_ref:
+                zip_ref.extractall(storage_location)
+    
+
 def run():
     print("Acquire: downloading raw data files...")
+    
     #Create api instance object
     api = KaggleApi()
     #Run authentication method so that the instance is authenticated
@@ -16,19 +42,7 @@ def run():
     storage_location = 'data/raw'
     
     files_to_download = ['train_labels.csv', 'train_data.csv']
-
-    for file in files_to_download:
-
-        api.competition_download_file(
-            competition='amex-default-prediction',
-            file_name=file,
-            path=storage_location
-        )
-
-        if not os.path.isfile(storage_location + '/' + file): 
-
-            path_to_zip_file = storage_location + '/' + file + '.zip'
-
-            with zipfile.ZipFile(path_to_zip_file, 'r') as zip_ref:
-                zip_ref.extractall(storage_location)
+    
+    download_files(api, files_to_download, storage_location)
+    
     print("Acquire: Completed!")
