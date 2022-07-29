ea0b0ef2d9bb5169b228dc2abffed642b732b99b
Adam Krull <adam.krull@codeup.com>
2022-07-29 17:04:26 -0500

    finalized exploration

--- notebooks/ak/myfunctions.py
+++ notebooks/ak/myfunctions.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import datetime as dt
 
 def amex_metric(y_true: pd.DataFrame, y_pred: pd.DataFrame) -> float:
 
@@ -30,3 +31,42 @@ def amex_metric(y_true: pd.DataFrame, y_pred: pd.DataFrame) -> float:
     d = top_four_percent_captured(y_true, y_pred)
 
     return 0.5 * (g + d)
+
+
+def age_calculator(df):
+    
+    def datetimer(string):
+        return dt.datetime.strptime(string, '%Y-%m-%d')
+    
+    df['S_2'] = df['S_2'].apply(datetimer)
+    
+    min_dates = pd.DataFrame(df.groupby('customer_ID').S_2.min())
+                             
+    min_dates.rename(columns={'S_2': 'min_dates'}, inplace=True)
+                             
+    df = df.merge(min_dates, how='left', on='customer_ID')
+                             
+    df['age'] = (df['S_2'] - df['min_dates']).dt.days
+                             
+    df.drop(columns=['S_2', 'min_dates'], inplace=True)
+                             
+    return df
+                             
+
+def value_haver(train, col_list):
+    
+    df = pd.DataFrame(index=train['customer_ID'].unique())
+    
+    for col in col_list:
+        df[col + '_has_value'] = train.groupby('customer_ID')[col].sum()
+        df[col + '_has_value'].where(df[col + '_has_value'] == 0, other=1, inplace=True)
+                             
+    return df
+                             
+                             
+def changefinder(df, train, col_list):
+                             
+    for col in col_list:
+        df[col + '_change'] = train[train.groupby('customer_ID')['age'].transform(max) == train['age']][col].to_numpy() -                                               train[train.groupby('customer_ID')['age'].transform(min) == train['age']][col].to_numpy()
+                             
+    return df                         
