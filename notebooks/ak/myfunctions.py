bbb26d125f0204d4d1e7de07901f0b8d96f564ea
Adam Krull <adam.krull@codeup.com>
2022-08-05 16:59:51 -0500

    additional feature engineering and boosting

--- notebooks/ak/myfunctions.py
+++ notebooks/ak/myfunctions.py
@@ -1,5 +1,6 @@
 import pandas as pd
-import datetime as dt
+import datetime as dtq
+from sklearn.preprocessing import StandardScaler
 
 def amex_metric(y_true: pd.DataFrame, y_pred: pd.DataFrame) -> float:
 
@@ -51,22 +52,39 @@ def age_calculator(df):
     df.drop(columns=['S_2', 'min_dates'], inplace=True)
                              
     return df
-                             
 
-def value_haver(train, col_list):
+
+def initial_prep(df):
     
-    df = pd.DataFrame(index=train['customer_ID'].unique())
+    df.drop(columns=['S_2'], inplace=True)
     
-    for col in col_list:
-        df[col + '_has_value'] = train.groupby('customer_ID')[col].sum()
-        df[col + '_has_value'].where(df[col + '_has_value'] == 0, other=1, inplace=True)
-                             
     return df
                              
-                             
-def changefinder(df, train, col_list):
-                             
-    for col in col_list:
-        df[col + '_change'] = train[train.groupby('customer_ID')['age'].transform(max) == train['age']][col].to_numpy() -                                               train[train.groupby('customer_ID')['age'].transform(min) == train['age']][col].to_numpy()
-                             
-    return df                         
+
+def null_counter(df):
+    
+    df_count = df.groupby('customer_ID').agg(lambda x: x.isnull().sum())
+    
+    df_count.columns = [x + '_nulls' for x in df_count.columns]
+    
+    ss = StandardScaler()
+    
+    df_count[df_count.columns] = ss.fit_transform(df_count[df_count.columns])
+    
+    return df_count, ss
+    
+
+def handle_categories(df):
+    
+    categorical_columns = ['B_30', 'B_38', 'D_114', 'D_116', 'D_117', 'D_120', 'D_126', 'D_63', 'D_64', 'D_66', 'D_68']
+    
+    dummy_df = pd.get_dummies(df[categorical_columns], dummy_na=True)
+    
+    dummy_last = dummy_df.groupby('customer_ID').agg('last')
+    
+    return dummy_last
+
+
+def cap_numerical_columns(df):
+    
+    numerical_columns = 
